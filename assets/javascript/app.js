698fada467ecf98c0eab65f7e63566db1a3f4a3b
Digital Samsquanch <digital.samsquanch@protonmail.com>
2019-09-04 16:01:23 -0400

    created html skeleton and pseudocoded the javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+/*
+- create object with all the questions
+    - each question will have an index, the actual question itself, possible answers, and the correct answer.
+- create startGame function that will fill in the html template with the question and the possible answers (potentially randomized order to prevent cheating)
+    - make a timer start as soon as the game starts
+- player must either select an answer and submit to move on to the next question or have the time run out and move on to the next question
+- the correct/incorrect message will render for 5 seconds and the correct answer will render regardless
+    
+*/
